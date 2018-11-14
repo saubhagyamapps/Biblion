1237f430a0db0b102b283e46058262c671c7450e
Payal Mavani <38760896+payalmavani007@users.noreply.github.com>
2018-11-14 10:51:00 +0530

    login

---
+++ app/src/main/java/app/biblion/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package app.biblion.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.biblion.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
